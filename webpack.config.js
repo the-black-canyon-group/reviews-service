3b6b7a4bc629d9ae4fb5fa61ffb126b709951d7f
kaylatillman <kaylatillman319@gmail.com>
2019-08-26 14:05:32 -0700

    initial package.json/package.lock

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+module.exports = {
+  entry: './client/index.jsx',
+  module: {
+    rules: [{
+      test: [/\.jsx$/],
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react', '@babel/preset-env']
+        }
+      }
+    }
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  }
+}
