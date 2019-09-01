9d719fcacd5ccff2d0f7c2068bf4d35648b58113
kaylatillman <kaylatillman319@gmail.com>
2019-08-31 17:40:32 -0700

    Rendered SearchBar

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,6 @@ class App extends React.Component {
       }
       this.search = this.search.bind(this)
       this.clearSearch = this.clearSearch.bind(this)
-      // this.filterz = this.filterReviewsBySearchTerm.bind(this)
-      this.sendReviews = this.sendReviews.bind(this)
-
     }
 
     componentDidMount() {
@@ -32,40 +29,28 @@ class App extends React.Component {
         });
     }
 
-  //PROP TYPS TO ===> REVIEWLIST
   search(searchedTerm) {
     for (var i = 0; i < this.state.reviews.length; i += 1) {
       //LOOK THROUGH ALL REVIEWS ==> FIND INDIVISUAL REVIEW TEXT 
       if (this.state.reviews[i].review.includes(searchedTerm)) {
         return reviews[i]
       } else {
+        this.setState({searchedTerm: searchedTerm})
         return <h2>None of our guests have mentioned {this.searchedTerm}</h2>
       }
     }
-    this.setState({searchedTerm: searchedTerm})
   }
 
   clearSearch() {
     this.setState({ searchedTerm: null })
   }
 
-  sendReviews() { 
-    this.state.reviews
-  }
-
-  // filterReviewsBySearchTerm() {
-  //     if (this.state.searchedTerm !== null) {
-  //       return this.search(searchedTerm)
-  //     } else {
-  //       return this.state.reviews;
-  //     }
-  // }
 
   render() {    
     return (
       <div>
-        <SearchBar search={this.search}/>
-        <ReviewList reviews={this.sendReviews}/>
+        <SearchBar search={this.search} clearSearch={this.clearSearch}/>
+        <ReviewList reviews={this.state.reviews}/>
       </div>
     );
   }
